e62b33ab48e2e15c6e486273ccc35c69ab764cc6
Praveenkumar <praveenkumar.p268@gmail.com>
2019-11-26 12:41:35 +0800

    Test Azure Devops

---
+++ src/webparts/knockoutWebpart/KnockoutWebpart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./KnockoutWebpart.module.css");
+const styles = {
+  knockoutWebpart: 'knockoutWebpart_c05211ae',
+  container: 'container_c05211ae',
+  row: 'row_c05211ae',
+  column: 'column_c05211ae',
+  'ms-Grid': 'ms-Grid_c05211ae',
+  title: 'title_c05211ae',
+  subTitle: 'subTitle_c05211ae',
+  description: 'description_c05211ae',
+  button: 'button_c05211ae',
+  label: 'label_c05211ae'
+};
+
+export default styles;
+/* tslint:enable */
---
+++ src/webparts/knockoutWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IKnockoutWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  ListNameFieldLabel: string;
+}
+
+declare module 'KnockoutWebpartWebPartStrings' {
+  const strings: IKnockoutWebpartWebPartStrings;
+  export = strings;
+}
